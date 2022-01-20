a38d2a3a77343078303df735ec18cc08bfd170da
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2022-01-19 22:23:58 -0800

    updated styles
    destructured pokemon props, display max 9 pokemon to reduce load times

--- pokedex.js
+++ pokedex.js
@@ -1,34 +1,37 @@
 const pokedex = document.getElementById('container');
 
 const createCard = (pokemon) => {
+    const { id, name, sprites: {other: {home: {front_default: image}}}, types, height, weight } = pokemon;
     return (
         `<div class="card">
-                <p class="id">${pokemon.id}</p>
-                <h3>${pokemon.name}</h3>
+                <aside class="poke-detail id">${id}</aside>
+                <h3 class="poke-detail name">${name}</h3>
                 <div class="img-container">
-                    <img src="${pokemon.sprites.other.home.front_default}" alt="">
+                    <img src="${image}" alt="image of ${name}">
                 </div>
-                <p class="poke-type">Types: ${pokemon.types.map(type => type.type.name).join(', ')}</p>
-                <p class="height">Height: ${pokemon.height}</p>
-                <p class="weight">Weight: ${pokemon.weight}</p>
-        </div>`
+                <div class="poke-detail type">Types: ${types.map(type => type.type.name).join(', ')}</div>
+                <div class="poke-detail height">Height: ${height}</div>
+                <div class="poke-detail weight">Weight: ${weight}</div>
+        </div>
+        `
     )
 }
 
 const fetchPokemon = () => {
     const url = 'https://pokeapi.co/api/v2/pokemon/';
     const proms = []
-    for (let i = 1; i <= 151; i++) {
+    for (let i = 1; i <= 9; i++) {
         const prom = fetch(`${url}${i}`).then(res => res.json());
         proms.push(prom)
     }
     
     Promise.all(proms).then((pokedata) => {
         pokedata.forEach(pokemon => {
+            console.log(pokemon)
             pokedex.insertAdjacentHTML("beforeend", createCard(pokemon));
         })
     });
 
 }
 
-fetchPokemon()
+fetchPokemon();
